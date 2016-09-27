63dc31fea92284f815f93e8f0b4476bd23af0054
Haroen Viaene <hello@haroen.me>
2016-09-27 21:17:55 +0200

    algolia: post and delete queries

--- controllers/api/1/index.js
+++ controllers/api/1/index.js
@@ -1,17 +1,35 @@
 const express = require('express');
+const algoliasearch = require('algoliasearch');
 
 const router = new express.Router();
+var client = algoliasearch('XCG7JJBBFK', process.env.ALGOLIA_API_KEY);
+var index = client.initIndex('appstore');
 
 // post a new Object to Algolia
-router.post('/apps', function (req, res) {
-  // todo: post to Algolia
-  res.send('Cow, Horse, Sheep');
+router.post('/apps', (req, res) => {
+  try {
+    const body = JSON.parse(req.body);
+    index.addObject(body, (err, content) => {
+      if (err) {
+        res.send(err);
+      }
+      res.header('Content-Type', 'application/json');
+      res.send(content);
+    });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 // delete a certain id from Algolia
-router.delete('/apps/:id', function (req, res) {
-  // todo: delete from Algolia
-  res.send(req.params.id);
+router.delete('/apps/:id', (req, res) => {
+  index.deleteObject(req.params.id, err => {
+    if (err) {
+      res.header('Content-Type', 'application/json');
+      res.send(err);
+    }
+    res.send('deleted ' + req.params.id);
+  });
 });
 
 module.exports = router;
