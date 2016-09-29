13109f9b84798bc764112c9cf8ac9af41bdbf130
Haroen Viaene <hello@haroen.me>
2016-09-29 20:31:11 +0200

    allow heroku to set the port

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const port = process.env.PORT || 8080;
+
 const app = express();
 
 app.use(require('./controllers'));
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
 });
