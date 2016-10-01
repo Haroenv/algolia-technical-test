9674c4bc33ecc11cdac0eb5926195278c6d2bfd5
Haroen Viaene <hello@haroen.me>
2016-10-01 13:20:32 +0200

    hotfix: start Vue with new

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue';
 
 import App from './App.vue';
 
-// eslint-disable-next-line babel/new-cap
-Vue({
+// eslint-disable-next-line no-new
+new Vue({
   el: '#app',
   render: h => h(App)
 });
